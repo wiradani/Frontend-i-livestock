df3f49643e2ff6ea03ae69c2b0c71e1f19e97012
wiradani <adhan432@gmail.com>
2017-11-21 19:21:49 +0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Frontend-i-livestock
 SI project for FAPET IPB
+
+# Project-RPL-Order-Home
+
+This project to complete our assigment for Software Enginering
+1. AIR (frontend)
+
+2. ALvin  (frontend)
+
+3. Wiradani R (backend)
+4. Octavian (backend)
+
+#  Penting!!
+
+1. git clone link nya
+
+2. setelah git clone run 'npm install' buat install modul modul nya
+
+3. untuk ionic masuk ke folder,buka terminal run 'npm start'
+
+4. 'git add' untuk memasukan data
+
+5. git add .' masukan semua folder
+
+6. git commit "comemntar"'
+
+7. git push origin master' masukan ke git di web/atau origin "branch"
+
+# Penting lagi
+buat branch untuk mencegah konflik
+jangan upload ke master kalau belum final
---
+++ i-Livestock/src/app/layout/layout-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout.component';
+
+const routes: Routes = [
+    {
+        path: '', component: LayoutComponent,
+        children: [
+	    { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
+            { path: 'qurban_me', loadChildren: './qurban_me/dashboard.module#DashboardModule' },
+            { path: 'jumlah_hewan', loadChildren: './jumlah_hewan/charts.module#ChartsModule' },
+	        { path: 'jumlah_pakan', loadChildren: './jumlah_pakan/charts.module#ChartsModule' },
+	        { path: 'sapi', loadChildren: './sapi/charts.module#ChartsModule' },
+	        { path: 'kambing', loadChildren: './kambing/charts.module#ChartsModule' },
+            { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
+            { path: 'tambah_hewan', loadChildren: './tambah_hewan/form.module#FormModule' },
+            { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
+            { path: 'grid', loadChildren: './grid/grid.module#GridModule' },
+            { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
+            { path: 'tentang_kami', loadChildren: './tentang_kami/blank-page.module#BlankPageModule' },
+	    { path: 'bantuan', loadChildren: './bantuan/blank-page.module#BlankPageModule' },
+	    { path: 'profil', loadChildren: './profil/blank-page.module#BlankPageModule' },
+	    { path: 'kotak_masuk', loadChildren: './kotak_masuk/blank-page.module#BlankPageModule' },
+	    { path: 'pengaturan', loadChildren: './pengaturan/blank-page.module#BlankPageModule' }
+        ]
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class LayoutRoutingModule { }
